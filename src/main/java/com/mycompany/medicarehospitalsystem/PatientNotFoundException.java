d854ff93852b14cc329774019202f2cd32740eee
ntshembhof-bit <ntshembhof@gmail.com>
2026-08-22 12:02:33 +0200

    Added PatientNotAvailableIDException class

---
+++ src/main/java/com/mycompany/medicarehospitalsystem/PatientNotFoundException.java
@@ -0,0 +1,8 @@
+
+package com.mycompany.medicarehospitalsystem;
+
+public class PatientNotFoundException extends Exception{
+    public PatientNotFoundException(String message){
+    super(message);
+}
+}
