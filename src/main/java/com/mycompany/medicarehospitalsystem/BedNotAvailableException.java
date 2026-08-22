10acf153af6c355d0805a991b65c1b5c6b33288e
ntshembhof-bit <ntshembhof@gmail.com>
2026-08-22 12:55:52 +0200

    Added BedNotAvailableException class

---
+++ src/main/java/com/mycompany/medicarehospitalsystem/BedNotAvailableException.java
@@ -0,0 +1,8 @@
+
+package com.mycompany.medicarehospitalsystem;
+
+public class BedNotAvailableException extends Exception{
+    public BedNotAvailableException(String message){
+        super(message);
+    }
+}
